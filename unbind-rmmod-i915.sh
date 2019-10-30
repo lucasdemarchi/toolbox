3c36166c4768f2e3e31a9270363188c8b2bba6e7
Lucas De Marchi <lucas.demarchi@intel.com>
2019-10-30 10:00:47 -0700

    Get display controller from lspci

--- unbind-rmmod-i915.sh
+++ unbind-rmmod-i915.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-echo -n auto > /sys/bus/pci/devices/0000\:00\:02.0/power/control
-echo -n "0000:00:02.0" > /sys/bus/pci/drivers/i915/unbind
+display_controller_class="0300"
+# assume we have only one for now
+read dev xxx <<<$(lspci -d ::$display_controller_class)
+sysdev=/sys/bus/pci/devices/0000:$dev/power/control
+
+echo -n auto > ${sysdev}
+echo -n "0000:$dev" > /sys/bus/pci/drivers/i915/unbind
 modprobe -r i915
