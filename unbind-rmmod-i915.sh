01c2e1e4d13559e74b629bcf1a7cff70f1c8882e
Lucas De Marchi <lucas.demarchi@intel.com>
2021-07-19 11:48:13 -0700

    unbind-rmmod-i915.sh: unbind from all devices before removing
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>

--- unbind-rmmod-i915.sh
+++ unbind-rmmod-i915.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-display_controller_class="0300"
-# assume we have only one for now
-read dev xxx <<<$(lspci -d ::$display_controller_class)
-sysdev=/sys/bus/pci/devices/0000:$dev/power/control
+set -e
 
-echo -n auto > ${sysdev}
-echo -n "0000:$dev" > /sys/bus/pci/drivers/i915/unbind
+display_controller_class="0300"
+pci_vendor="8086"
+
+while read -r pci_slot class devid xxx; do
+	sysdev=/sys/bus/pci/devices/0000:$pci_slot/power/control
+
+	echo "Unbinding /sys/bus/pci/devices/0000:$pci_slot ($devid)"
+	echo -n auto > ${sysdev}
+	if ! echo -n "0000:$pci_slot" > /sys/bus/pci/drivers/i915/unbind; then
+		driver="$(basename $(readlink -f /sys/bus/pci/devices/0000:4d:00.0/driver))" || true
+		if [ $driver == i915 ]; then
+			echo "ERROR: could not unbind driver" > /dev/stderr
+		fi
+	fi
+done <<<$(lspci -d ${pci_vendor}::${display_controller_class} -n)
+
 modprobe -r i915
