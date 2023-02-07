f5869f24463bee4b12a2b3c3152cb9d3d5a2449b
Lucas De Marchi <lucas.demarchi@intel.com>
2023-02-07 11:32:21 -0800

    unbind-rmmod-i915.sh -> unbind-rmmod-gfx.sh
    Make script compatible with the xe driver.
    
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>

--- unbind-rmmod-gfx.sh
+++ unbind-rmmod-gfx.sh
@@ -6,17 +6,21 @@ vga="0300"
 display="0380"
 pci_vendor="8086"
 
+# pass "xe" as param to unbind/rmmod xe
+driver=${1:-i915}
+
 while read -r pci_slot class devid xxx; do
 	sysdev=/sys/bus/pci/devices/0000:$pci_slot/power/control
 
 	echo "Unbinding /sys/bus/pci/devices/0000:$pci_slot ($devid)"
 	echo -n auto > ${sysdev}
-	if ! echo -n "0000:$pci_slot" > /sys/bus/pci/drivers/i915/unbind; then
-		driver="$(basename $(readlink -f /sys/bus/pci/devices/0000:$pci_slot/driver))" || true
-		if [ $driver == i915 ]; then
-			echo "ERROR: could not unbind driver" > /dev/stderr
+
+	if ! echo -n "0000:$pci_slot" > /sys/bus/pci/drivers/$driver/unbind; then
+		d="$(basename $(readlink -f /sys/bus/pci/devices/0000:$pci_slot/driver))" || true
+		if [ $driver == $d ]; then
+			echo "ERROR: could not unbind $d driver" > /dev/stderr
 		fi
 	fi
 done <<<$(lspci -d ${pci_vendor}::${display} -n; lspci -d ${pci_vendor}::${vga} -n )
 
-modprobe -r i915
+modprobe -r $driver
